a8d71bdd3a1b3f7675ac67272844542515fa18b8
alek <wait82985@gmail.com>
2023-07-12 12:42:16 +0600

    add Github repo

---
+++ src/main/java/Solution.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        //TODO
+        return new ArrayList<>();
+    }
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SolutionTest {
+    @Test
+    public void eventualSafeNodesTest1() {
+        int[][] graph = {
+                {1, 2},
+                {2, 3},
+                {5},
+                {0},
+                {5},
+                {},
+                {}
+        };
+        List<Integer> expected = new ArrayList<>(List.of(2, 4, 5, 6));
+        List<Integer> actual = new Solution().eventualSafeNodes(graph);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void eventualSafeNodesTest2() {
+        int[][] graph = {
+                {1, 2, 3, 4},
+                {1, 2},
+                {3, 4},
+                {0, 4},
+                {5},
+                {}
+        };
+        List<Integer> expected = new ArrayList<>(List.of(4));
+        List<Integer> actual = new Solution().eventualSafeNodes(graph);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+}
